3b1ae59e14b2d1d366fcbf0b35c38fcf993364e6
ganeshsn <sganesh.narayanan@gmail.com>
2022-08-23 12:49:30 -0400

    Update ensure-server-setup.sh

--- module5_task0/ensure-server-setup.sh
+++ module5_task0/ensure-server-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 
+sudo yum install awscli
 EC2_INSTANCE=aws ec2 describe-instances --query "Reservations[].Instances[].PublicDnsName"
 
 #connect to the server and install docker 20.10
 #SCRIPT="sudo apt-get update; sudo apt-get install docker-ce=20.10; sudo systemctl start docker"
-ssh ubuntu@"$EC2_INSTANCE" uname
+ssh ec2-user@"$EC2_INSTANCE" uname
 
