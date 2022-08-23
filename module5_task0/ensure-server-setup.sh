6a4b8d640ed9e9d83d5c2d37f8e80f972c6c2bdd
ganeshsn <sganesh.narayanan@gmail.com>
2022-08-23 14:06:23 -0400

    Update ensure-server-setup.sh

--- module5_task0/ensure-server-setup.sh
+++ module5_task0/ensure-server-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
 sudo cat /etc/os-release
-sudo apt-get update
-sudo apt-get install -y awscli
+sudo apt-get install -y unzip
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 sudo unzip awscliv2.zip
 sudo ./aws/install
+
 EC2_INSTANCE=aws ec2 describe-instances --query "Reservations[].Instances[].PublicDnsName"
 
 #connect to the server and install docker 20.10
