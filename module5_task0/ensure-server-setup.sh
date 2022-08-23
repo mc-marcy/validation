d8f9723515506d9e2cf668bdae1008b7d34058d6
ganeshsn <sganesh.narayanan@gmail.com>
2022-08-22 21:07:29 -0400

    Update ensure-server-setup.sh

--- module5_task0/ensure-server-setup.sh
+++ module5_task0/ensure-server-setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash 
 
-if [ "$#" -ne 1 ]; then
-    echo "You must enter exactly 1 command line argument with ec2 instance public ip"
-    exit -1
-fi
-
-EC2_INSTANCE=$1
+EC2_INSTANCE=aws ec2 describe-instances --query "Reservations[].Instances[].PublicDnsName"
 
 #connect to the server and install docker 20.10
 #SCRIPT="sudo apt-get update; sudo apt-get install docker-ce=20.10; sudo systemctl start docker"
