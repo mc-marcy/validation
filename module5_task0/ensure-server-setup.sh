803d88120d1c5bd738afd36be2e3a32648ee6a15
ganeshsn <sganesh.narayanan@gmail.com>
2022-08-23 14:28:35 -0400

    Update ensure-server-setup.sh

--- module5_task0/ensure-server-setup.sh
+++ module5_task0/ensure-server-setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash 
-
-sudo cat /etc/os-release
 sudo apt-get install -y unzip
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
 sudo unzip awscliv2.zip
 sudo ./aws/install
 
-EC2_INSTANCE=aws ec2 describe-instances --query "Reservations[].Instances[].PublicDnsName"
-ssh ec2-user@"$EC2_INSTANCE uname"
+#EC2_INSTANCE=aws ec2 describe-instances --query "Reservations[].Instances[].PublicDnsName"
+ssh ec2-user@localhost
 
