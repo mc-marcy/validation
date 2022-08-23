fc9291b9f2e7ad1ac8fc8610b19a929532f6704b
ganeshsn <sganesh.narayanan@gmail.com>
2022-08-22 21:13:06 -0400

    Update ensure-server-setup.sh

--- module5_task0/ensure-server-setup.sh
+++ module5_task0/ensure-server-setup.sh
@@ -4,5 +4,5 @@ EC2_INSTANCE=aws ec2 describe-instances --query "Reservations[].Instances[].Publ
 
 #connect to the server and install docker 20.10
 #SCRIPT="sudo apt-get update; sudo apt-get install docker-ce=20.10; sudo systemctl start docker"
-ssh ubuntu@$EC2_INSTANCE uname
+ssh ubuntu@"$EC2_INSTANCE" uname
 
