9c211f4ee165ffddda159d6547934aa1e6fed0f7
ganeshsn <sganesh.narayanan@gmail.com>
2022-08-23 21:21:46 -0400

    Update ensure-server-created.sh

--- module5_task0/ensure-server-created.sh
+++ module5_task0/ensure-server-created.sh
@@ -10,7 +10,7 @@ instance=$(aws ec2 describe-instances \
 
 if [ -z "$instance" ]
 then
-  echo "no ec2 instances of type t3.micro is running in us-east-1"
+  #echo "no ec2 instances of type t3.micro is running in us-east-1"
   aws ec2 run-instances --image-id $AMI --count 1 --instance-type t3.micro --key-name awesome-key  --security-group-ids $SG
   #aws ec2 describe-instances --query "Reservations[].Instances[].PublicDnsName"
   
