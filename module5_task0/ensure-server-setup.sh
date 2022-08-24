9360f7dc31617090b770d552681e7f99c42af321
ganeshsn <sganesh.narayanan@gmail.com>
2022-08-23 20:26:56 -0400

    Update ensure-server-setup.sh

--- module5_task0/ensure-server-setup.sh
+++ module5_task0/ensure-server-setup.sh
@@ -1,9 +1,20 @@
-#!/bin/bash 
-sudo apt-get install -y unzip
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
-sudo unzip -q -o awscliv2.zip
-sudo ./aws/install
+#!/bin/bash
 
 EC2_INSTANCE=$(aws ec2 describe-instances --query "Reservations[].Instances[].PublicIpAddress" --output text)
-ssh ubuntu@"$EC2_INSTANCE"
+#sudo mkdir -p /etc/apt/keyrings
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
+ssh ubuntu@"$EC2_INSTANCE" << EOF
+   sudo apt-get update
+   sudo apt-get -y install ca-certificates curl gnupg lsb-release
+
+   echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+   sudo apt-get update
+   sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+   sudo systemctl start docker
+   sudo usermod -aG docker ubuntu
+EOF
